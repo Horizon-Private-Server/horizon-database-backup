70afeb653949c7002141d7270b38b6b45c522f3e
John Janecek <janecektyler@gmail.com>
2024-06-11 20:33:10 -0500

    Add restore function

--- backup.sh
+++ backup.sh
@@ -13,9 +13,28 @@ TIMESTAMP=$(date +%Y%m%d%H%M%S)
 # Generate the backup file path with the timestamp
 BACKUP_FILE="/backup/horizon_db_${TIMESTAMP}.bak"
 
-echo "Creating backup ..."
+# Determine action based on MODE
+if [ "$MODE" == "backup" ]; then
+    echo "Creating backup ..."
 
-# Run the backup command
-/opt/mssql-tools/bin/sqlcmd -S $SERVER_NAME -U $SQLCMD_USER -P $SQLCMD_PASSWORD -Q "BACKUP DATABASE [$DATABASE_NAME] TO DISK = N'$BACKUP_FILE' WITH NOFORMAT, NOINIT, NAME = '$DATABASE_NAME-Full Database Backup', SKIP, NOREWIND, NOUNLOAD, STATS = 10"
+    # Run the backup command
+    /opt/mssql-tools/bin/sqlcmd -S $SERVER_NAME -U $SQLCMD_USER -P $SQLCMD_PASSWORD -Q "BACKUP DATABASE [$DATABASE_NAME] TO DISK = N'$BACKUP_FILE' WITH NOFORMAT, NOINIT, NAME = '$DATABASE_NAME-Full Database Backup', SKIP, NOREWIND, NOUNLOAD, STATS = 10"
 
-echo "Backup completed: $BACKUP_FILE"
+    echo "Backup completed: $BACKUP_FILE"
+
+elif [ "$MODE" == "restore" ]; then
+    # Check if RESTORE_FILE is set
+    if [ -z "$RESTORE_FILE" ]; then
+        echo "RESTORE_FILE environment variable is not set."
+        exit 1
+    fi
+
+    # Run the restore command
+    /opt/mssql-tools/bin/sqlcmd -S $SERVER_NAME -U $SQLCMD_USER -P $SQLCMD_PASSWORD -Q "RESTORE DATABASE [$DATABASE_NAME] FROM DISK = N'$RESTORE_FILE' WITH FILE = 1, NOUNLOAD, REPLACE, STATS = 10"
+    
+    echo "Restore completed from: $RESTORE_FILE"
+
+else
+    echo "Invalid MODE: $MODE. It should be 'backup' or 'restore'."
+    exit 1
+fi
--- run_restore.sh
+++ run_restore.sh
@@ -7,7 +7,8 @@ docker run --rm \
     -e DATABASE_NAME=$HORIZON_DB_NAME \
     -e SQLCMD_USER=$HORIZON_DB_USER \
     -e SQLCMD_PASSWORD=$HORIZON_MSSQL_SA_PASSWORD \
-    -e BACKUP_PATH=/ \
+    -e RESTORE_FILE=/ \
+    -e MODE=restore \
     horizon-database-backup
 
 source /root/.bashrc && aws s3 sync $DATABASE_BACKUP_LOC $DATABASE_BACKUP_S3_DEST --exact-timestamps --no-progress
