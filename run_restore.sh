de3e25ffe8b7419615b4d9a3ed4c0a6ed69be6ce
John Janecek <janecektyler@gmail.com>
2024-06-11 22:20:24 -0500

    Fix run restore function

--- run_restore.sh
+++ run_restore.sh
@@ -7,8 +7,6 @@ docker run --rm \
     -e DATABASE_NAME=$HORIZON_DB_NAME \
     -e SQLCMD_USER=$HORIZON_DB_USER \
     -e SQLCMD_PASSWORD=$HORIZON_MSSQL_SA_PASSWORD \
-    -e RESTORE_FILE=/ \
+    -e RESTORE_FILE=/backup/horizon_db_20240611000617.bak \
     -e MODE=restore \
     horizon-database-backup
-
-source /root/.bashrc && aws s3 sync $DATABASE_BACKUP_LOC $DATABASE_BACKUP_S3_DEST --exact-timestamps --no-progress
